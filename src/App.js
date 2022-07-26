0bed1a0b3787e27e0af7f70f5151a3fce506f14c
olexandr.Tsp <megabat@ukr.net>
2022-07-27 00:12:36 +0300

    Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import './scss/app.scss'
+import Header from './Components/Header';
+import Categories from './Components/Categories';
+import Sort from './Components/Sort';
+import PizzaBlock from './Components/PizzaBlock';
 
-import './App.css';
 
 function App() {
   return (
-    <>
-
-    </>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title='Pepperoni ' price='555' />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
