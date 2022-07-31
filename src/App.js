82b6ba04a666fd2d47f2ca4cfffb7a0215626a28
olexandr.Tsp <megabat@ukr.net>
2022-07-31 18:19:11 +0300

    mockApi

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import Header from './Components/Header';
 import Categories from './Components/Categories';
 import Sort from './Components/Sort';
 import PizzaBlock from './Components/PizzaBlock';
-import pizzas from './assets/db.json'
+import { useEffect, useState } from 'react';
 
+// https://62e694a269bd03090f72e797.mockapi.io/items
 
 function App() {
+
+  const [items, SetItems] = useState([])
+
+  useEffect(() => {
+    fetch('https://62e694a269bd03090f72e797.mockapi.io/items')
+      .then((res) => {
+        return res.json()
+      })
+      .then((json) => {
+        SetItems(json)
+      })
+  }, [])
+
   return (
     <div className="wrapper">
       <Header />
@@ -18,7 +32,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => {
+            {items.map((obj) => {
               return <PizzaBlock key={obj.id} {...obj} />
             })}
           </div>
