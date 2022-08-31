98dffecab0269e5d073ee733d34b055bd6d4b185
olexandr.Tsp <megabat@ukr.net>
2022-09-01 00:40:21 +0300

    card

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom';
 import Search from './Search';
 import { useSelector } from 'react-redux';
 
-
 export default function Header() {
-  const { items, totalPrice } = useSelector(state => state.cart);
+  const { items, totalPrice } = useSelector((state) => state.cart);
+
+  const totalCount = items.reduce((sum, item) => sum + item.count, 0);
 
   return (
     <div className="header">
@@ -52,7 +53,7 @@ export default function Header() {
                 strokeLinejoin="round"
               />
             </svg>
-            <span>{items.length}</span>
+            <span>{totalCount}</span>
           </Link>
         </div>
       </div>
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -9,13 +9,9 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    // addItem(state, action) {
-    //   state.items.push(action.payload);
-    //   state.totalPrice = state.items.reduce((sum, obj) => {
-    //     return obj.price + sum;
-    //   }, 0);
     addItem(state, action) {
       const findItem = state.items.find((obj) => obj.id === action.payload.id);
+
       if (findItem) findItem.count++;
       else {
         state.items.push({
@@ -24,18 +20,27 @@ const cartSlice = createSlice({
         });
       }
       state.totalPrice = state.items.reduce((sum, obj) => {
-        return obj.price + sum;
+        return obj.price * obj.count + sum;
       }, 0);
     },
     removeItem(state, action) {
-      state.items = state.items.filter((obj) => obj !== action.payload);
+      state.items = state.items.filter((obj) => obj.id !== action.payload);
     },
+
+    minusItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload);
+
+      if (findItem) {
+        findItem.count--;
+      }
+    },
+
     clearItem(state, action) {
       state.items = [];
     },
   },
 });
 
-export const { addItem, removeItem, clearItem } = cartSlice.actions;
+export const { addItem, removeItem, clearItem, minusItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
